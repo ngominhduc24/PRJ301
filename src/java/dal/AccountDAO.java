bf7637d010203474c9fc5b09d49a3a873a7389f4
Minh Duc <ngominhduc100@gmail.com>
2023-05-24 16:43:51 +0700

    checkAccount DAO

--- src/java/dal/AccountDAO.java
+++ src/java/dal/AccountDAO.java
@@ -4,10 +4,45 @@
  */
 package dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import model.Account;
+
 /**
  *
  * @author ASUS PC
  */
 public class AccountDAO {
-    
+    public Account checkAccount(String username, String password) {
+        String query = "SELECT * FROM Account WHERE username = ? AND password = ?";
+        try {
+            PreparedStatement ps = DbContext.getConnection().prepareStatement(query);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Account account = new Account();
+                account.setAccountID(rs.getInt("accountID"));
+                account.setEmail(rs.getString("email"));
+                account.setPassword(rs.getString("password"));
+                account.setName(rs.getString("name"));
+                account.setPhone(rs.getString("phone"));
+                account.setAddress(rs.getString("address"));
+                account.setRole(rs.getInt("role"));
+                return account;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                DbContext.getConnection().close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+        return null;
+    }
 }
