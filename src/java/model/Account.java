f4568a88f2f972f973cabb5b595fb031863baa75
Minh Duc <ngominhduc100@gmail.com>
2023-05-24 11:14:28 +0700

    create model Account

---
+++ src/java/model/Account.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ASUS PC
+ */
+public class Account {
+    private int accountID;
+    private String email;
+    private String password;
+    private String name;
+    private String phone;
+    private String address;
+    private int role;
+
+    public Account() {
+    }
+
+    public Account(int accountID, String email, String password, String name, String phone, String address, int role) {
+        this.accountID = accountID;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.role = role;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "accountID=" + accountID + ", email=" + email + ", password=" + password + ", name=" + name
+                + ", phone=" + phone + ", address=" + address + ", role=" + role + '}';
+    }
+
+}
