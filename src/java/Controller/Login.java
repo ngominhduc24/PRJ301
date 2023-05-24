ddc9184a37cc990c10ee1257d4ff86e3154afa76
Minh Duc <ngominhduc100@gmail.com>
2023-05-24 11:43:44 +0700

    add login controller

---
+++ src/java/Controller/Login.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Controller;
+
+import jakarta.servlet.ServletContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ASUS PC
+ */
+
+@WebServlet(name = "Login", urlPatterns = { "/login" })
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     * 
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Login</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+    // + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     * 
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     * 
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        // try (PrintWriter out = response.getWriter()) {
+        // String username = request.getParameter("Username");
+        // String password = request.getParameter("Password");
+        // // get init context
+        // String init_username = getServletContext().getInitParameter("username");
+        // String init_password = getServletContext().getInitParameter("password");
+        // if (username.equals(init_username) && password.equals(init_password)) {
+        // request.setAttribute("name", username);
+        // request.getRequestDispatcher("/view/welcome.jsp").forward(request, response);
+        // // out.println("login success");
+
+        // } else {
+        // request.setAttribute("error", "moi ban nhap lai");
+        // request.getRequestDispatcher("/view/login.jsp").forward(request, response);
+        // }
+        // }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     * 
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/login.jsp
@@ -0,0 +1,36 @@
+<%-- Document : login Created on : May 18, 2023, 11:04:29 AM Author : ASUS PC
+--%> <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="${pageContext.request.contextPath}/styles/login.css"
+    />
+    <title>Login Page</title>
+  </head>
+  <body>
+    <form action="login" method="post">
+      <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" name="Username" required />
+
+        <label for="psw"
+          ><b><br />Password</b></label
+        >
+        <input type="password" name="Password" required />
+
+        <button type="submit" id="loginbtn">Login</button>
+      </div>
+    </form>
+  </body>
+  <script>
+    var element = document.getElementById("loginbtn");
+    var msg = '<%= request.getAttribute("error") %>';
+    if (msg != "null") {
+      alert(msg);
+    }
+  </script>
+</html>
