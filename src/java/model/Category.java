7d29c0dfc8db155223b8a7a8000869dd44bb8910
Minh Duc <ngominhduc100@gmail.com>
2023-05-24 11:23:47 +0700

    create model Category

---
+++ src/java/model/Category.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ASUS PC
+ */
+public class Category {
+    // create table Category (
+    // CategoryID int not null primary key,
+    // Name varchar(50) not null
+    // );
+    private int categoryID;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(int categoryID, String name) {
+        this.categoryID = categoryID;
+        this.name = name;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" + "categoryID=" + categoryID + ", name=" + name + '}';
+    }
+}
